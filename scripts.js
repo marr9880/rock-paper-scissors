8d924917f0b1b4db93090abe7226db3aac35d042
Matt <229481176+marr9880@users.noreply.github.com>
2025-11-25 12:54:53 -0600

    Add buttons for rack, paper, and scissors player choices with Event listeners to call play round

--- scripts.js
+++ scripts.js
@@ -4,36 +4,52 @@ function getComputerChoice() {
     return computerChoice;
 };
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Please enter rock, paper, or scissors.");
-    return playerChoice.toLowerCase();
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "paper" || 
+        playerChoice === "paper" && computerChoice === "scissors" ||
+        playerChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beats ${playerChoice}.`);
+    } else if (playerChoice === "rock" && computerChoice === "scissors" ||
+               playerChoice === "paper" && computerChoice === "rock" ||
+               playerChoice === "scissors" && computerChoice === "paper") {
+               playerScore++;
+               console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
+    } else if (playerChoice === computerChoice) {
+               console.log("Tie! You both picked the same thing.");
+    }
+
+    console.log(computerScore);
+    console.log(playerScore);
 };
 
-function playGame() {
-    function playRound(playerChoice, computerChoice) {
-        if (playerChoice === "rock" && computerChoice === "paper" || 
-            playerChoice === "paper" && computerChoice === "scissors" ||
-            playerChoice === "scissors" && computerChoice === "rock") {
-            computerScore++;
-            console.log(`You lose! ${computerChoice} beats ${playerChoice}.`);
-        } else if (playerChoice === "rock" && computerChoice === "scissors" ||
-                   playerChoice === "paper" && computerChoice === "rock" ||
-                   playerChoice === "scissors" && computerChoice === "paper") {
-            playerScore++;
-            console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
-        } else if (playerChoice === computerChoice) {
-            console.log("Tie! You both picked the same thing.");
-        }
-    };
-    
-    let playerScore = 0;
-    let computerScore = 0;
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
 
-    
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-
+btnRock.addEventListener('click', () => {
+    let playerSelection = "rock";
     playRound(playerSelection, computerSelection);
+});
+
+btnPaper.addEventListener('click', () => {
+    let playerSelection = "paper";
+    playRound(playerSelection, computerSelection);
+});
+    
+btnScissors.addEventListener('click', () => {
+    let playerSelection = "scissors";
+    playRound(playerSelection, computerSelection);
+});    
+
+    
+    
+    
+
+    
 
     function declareScore() {
         if (playerScore > computerScore) {
@@ -46,7 +62,6 @@ function playGame() {
     };
 
     declareScore();
-};
 
 playGame();
 
